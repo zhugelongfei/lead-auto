5196715e8b88e0af6cd790336c4f729ebfc1deb7
Lonfee <18519720472@163.com>
2021-11-27 09:57:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone http://www.lonfee.cn:8002/ddns-script-aliyun.git package/ddns-script-aliyun/
